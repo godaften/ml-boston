7cc02b9674635edceb20a4307b34597503390c4b
NicklasSpendler <KN6s~?c$%+T>
2022-04-06 19:40:14 +0200

    henter scaler ned fra collab

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from statistics import mode
 from flask import Flask, render_template, request
 from tensorflow.keras.models import load_model
-#from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
+from pickle import load
 import numpy as np
 app = Flask(__name__)
 
 @app.route('/',methods=['post','get']) # will use get for the first page-load, post for the form-submit
 def predict(): # this function can have any name
     model = load_model('mymodel.h5') # the mymodel.h5 file was created in Colab, downloaded and uploaded using Filezilla
+    scaler = load(open('scaler.pkl', 'rb'))
+
     in1 = request.form.get('in1') # get the two numbers from the request object
     in2 = request.form.get('in2')
     in3 = request.form.get('in3')
@@ -15,9 +18,12 @@ def predict(): # this function can have any name
         return render_template('index.html', result='No input(s)')
         # calling render_template will inject the variable 'result' and send index.html to the browser
     else:
-        arr = np.array([[ float(in1),float(in2), float(in3) ]]) # cast string to decimal number, and make 2d numpy array.
-        predictions = model.predict(arr) # make new prediction
-        return render_template('index.html', result=str(predictions[0][0]))
+        #arr = np.array([[ float(in1),float(in2), float(in3) ]]) # cast string to decimal number, and make 2d numpy array.
+        
+        scaledPredictions = scaler.transform([[float(in1),float(in2),float(in3)]]);
+        predictions = model.predict(scaledPredictions) # make new prediction
+        #print(scaledPredictions)
+        return render_template('index.html', result=str(round(predictions[0][0] * 10000000, 2)))
         # the result is set, by asking for row=0, column=0. Then cast to string.
 
 if __name__ == '__main__':
